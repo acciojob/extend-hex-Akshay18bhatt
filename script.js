6635861befebd226dab79c4c2ca23ef8465fa93d
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-09 12:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const arr=[];
+	for(let i=0;i<shortHex.length;i++){
+		const ch= shortHex[i];
+		if(ch!='#'){
+			arr.push(ch+ch);
+		}
+	}
+	// console.log(arr);
+	let ans_str= "#"+arr.join("");
+	return ans_str;
+			   
+	
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex))
+
